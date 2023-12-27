3e924dfac52da91c6266850e0d3c1191b9eb9b41
doinel1a <doinel1atanasiu@gmail.com>
2023-12-27 15:23:33 +0100

    chore: configure test env

---
+++ _config.ts
@@ -0,0 +1,6 @@
+export default {
+  server: {
+    host: '127.0.0.1',
+    port: 5137
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { partytownVite } from '@builder.io/partytown/utils';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
+import _config from './_config';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -17,4 +19,8 @@ export default defineConfig({
       dest: path.join(__dirname, 'dist', '~partytown')
     })
   ],
+  server: {
+    host: _config.server.host,
+    port: _config.server.port
+  }
 });
