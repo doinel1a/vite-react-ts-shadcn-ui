9487ab6eade3bb127051736d00d9f61a37498d01
doinel1a <doinel1atanasiu@gmail.com>
2024-01-02 16:44:32 +0100

    refactor: switch to typescript config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from 'tailwindcss';
+
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -74,4 +75,4 @@ module.exports = {
     }
   },
   plugins: [require('tailwindcss-animate')]
-};
+} satisfies Config;
