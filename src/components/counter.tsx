07541aaae99836a8ed0310a68662a579a6e7250a
doinel1a <doinel1atanasiu@gmail.com>
2024-01-02 19:07:55 +0100

    refactor: ui

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button';
 import { Card, CardContent, CardFooter, CardHeader } from './ui/card';
 
 export default function Counter() {
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState(0);
 
   return (
     <Card className='border border-border bg-secondary shadow-md'>
@@ -12,27 +12,27 @@ export default function Counter() {
         <h1 className='text-2xl'>Vite React TypeScript</h1>
         <h2 className='text-lg'>with shadcn/ui</h2>
       </CardHeader>
-      <CardContent className='flex items-center justify-center'>
-        <h2 data-testid='count' className='text-6xl'>
-          {count}
-        </h2>
-      </CardContent>
-      <CardFooter className='flex items-center justify-evenly'>
+      <CardContent className='flex w-full items-center justify-between'>
         <Button
+          className='w-10 rounded-full'
           data-testid='increase-count'
-          className='w-16'
           onClick={() => setCount((previousCount) => previousCount + 1)}
         >
           + 1
         </Button>
+
+        <h2 className='text-6xl' data-testid='count'>
+          {count}
+        </h2>
+
         <Button
+          className='w-10 rounded-full'
           data-testid='decrease-count'
-          className='w-16'
           onClick={() => setCount((previousCount) => previousCount - 1)}
         >
           - 1
         </Button>
-      </CardFooter>
+      </CardContent>
     </Card>
   );
 }
