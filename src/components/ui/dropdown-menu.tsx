ce470ce5502d2150c03ccc92ec9991f1afcb0494
doinel1a <doinel1atanasiu@gmail.com>
2026-01-04 21:12:33 +0100

    refactor: swap lucide for tabler icons

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -4,7 +4,7 @@
 import * as React from 'react';
 
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
-import { CheckIcon, ChevronRightIcon, CircleIcon } from 'lucide-react';
+import { IconCheck, IconChevronRight, IconCircle } from '@tabler/icons-react';
 
 import { cn } from '@/lib/utils';
 
@@ -89,7 +89,7 @@ function DropdownMenuCheckboxItem({
     >
       <span className='pointer-events-none absolute left-2 flex size-3.5 items-center justify-center'>
         <DropdownMenuPrimitive.ItemIndicator>
-          <CheckIcon className='size-4' />
+          <IconCheck className='size-4' />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
       {children}
@@ -119,7 +119,7 @@ function DropdownMenuRadioItem({
     >
       <span className='pointer-events-none absolute left-2 flex size-3.5 items-center justify-center'>
         <DropdownMenuPrimitive.ItemIndicator>
-          <CircleIcon className='size-2 fill-current' />
+          <IconCircle className='size-2 fill-current' />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
       {children}
@@ -190,7 +190,7 @@ function DropdownMenuSubTrigger({
       {...props}
     >
       {children}
-      <ChevronRightIcon className='ml-auto size-4' />
+      <IconChevronRight className='ml-auto size-4' />
     </DropdownMenuPrimitive.SubTrigger>
   );
 }
